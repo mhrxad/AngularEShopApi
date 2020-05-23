b8bbaec0a0cef244ee9b8708e705e1c7b8175a26
mhrxadev <60345056+mhrxadev@users.noreply.github.com>
2020-05-23 14:50:52 +0430

    Initial slider & products Services

---
+++ AngularEshop.Core/Services/Interfaces/IProductService.cs
@@ -0,0 +1,18 @@
+﻿using AngularEshop.DataLayer.Entities.Product;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AngularEshop.Core.Services.Interfaces
+{
+    public interface IProductService : IDisposable
+    {
+        #region product
+
+        Task AddProduct(Product product);
+        Task UpdateProduct(Product product);
+
+        #endregion
+    }
+}
